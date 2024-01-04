5220355ded2a03b5232afa46b4405d78041901ed
David A. Chentes <davidalejandrochentes@gmail.com>
2024-01-04 12:11:25 -0500

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,8 @@
+from .serializer import TaskSerializer
+from .models import Task
+from rest_framework import viewsets
+
+# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
